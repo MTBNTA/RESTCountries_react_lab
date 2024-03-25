d5ffc947392afcf51fd7eae9392bc43f97db8e23
Maya T <tettehmaya5@gmail.com>
2024-03-25 14:12:08 +0000

    Added to countries container file.

--- src/containers/CountriesContainer.js
+++ src/containers/CountriesContainer.js
@@ -1,6 +1,24 @@
+import { useState, useEffect } from "react";
+import CountriesList from "../components/CountriesList";
+
 const CountriesContainer = () => {
+
+    const [countries, setCountries] = useState([]);
+
+    const fetchCountries = async () => {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        const jsonData = await response.json();
+        setCountries(jsonData);
+    }
+
+    useEffect(() => {
+        fetchCountries();
+    }, []);
+
     return ( 
-        <></>
+        <>
+            <CountriesList countries={countries} />
+        </>
      );
 }
  
